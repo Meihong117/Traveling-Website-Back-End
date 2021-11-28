641f932e1f284f59394abe5fe246f751d11620cb
Estelle <estelle.wu117@gmail.com>
2021-11-29 11:22:50 +1300

    change user and password

--- config/db.js
+++ config/db.js
@@ -5,10 +5,10 @@ let pool = null;
 exports.createPool = async function () {
     pool = mysql.createPool({
         multipleStatements: true,
-        host: process.env.SENG365_MYSQL_HOST,
-        user: process.env.SENG365_MYSQL_USER,
-        password: process.env.SENG365_MYSQL_PASSWORD,
-        database: process.env.SENG365_MYSQL_DATABASE
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
     });
 };
 
